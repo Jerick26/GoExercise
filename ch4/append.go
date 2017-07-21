f3c107fb11503d2f96fc4b77214f6879392eaa3e
jerry.pin26 <jerry.pin26@yahoo.com>
2017-07-21 17:52:52 +0800

    chore(slices): add comment and output information

--- ch4/append.go
+++ ch4/append.go
@@ -1,3 +1,23 @@
+/******************************************************************************
+ *  Compilation:  go build append.go
+ *  Execution:    ./append
+ *
+ *  append a int to int slice with doubling capacity when full
+ *
+ *  % ./append
+ *  0  cap=1	[0]
+ *  1  cap=2	[0 1]
+ *  2  cap=4	[0 1 2]
+ *  3  cap=4	[0 1 2 3]
+ *  4  cap=8	[0 1 2 3 4]
+ *  5  cap=8	[0 1 2 3 4 5]
+ *  6  cap=8	[0 1 2 3 4 5 6]
+ *  7  cap=8	[0 1 2 3 4 5 6 7]
+ *  8  cap=16	[0 1 2 3 4 5 6 7 8]
+ *  9  cap=16	[0 1 2 3 4 5 6 7 8 9]
+ *
+ ******************************************************************************/
+
 package main
 
 import "fmt"
@@ -17,7 +37,6 @@ func appendInt(x []int, y int) []int {
 		}
 		z = make([]int, zlen, zcap)
 		copy(z, x)
-		fmt.Printf("resize, zlen: %d, zcap: %d\n", zlen, zcap)
 	}
 	z[len(x)] = y
 	return z
@@ -27,6 +46,6 @@ func main() {
 	var a []int
 	for i := 0; i < 10; i++ {
 		a = appendInt(a, i)
+		fmt.Printf("%d  cap=%d\t%v\n", i, cap(a), a)
 	}
-	fmt.Println(a)
 }
