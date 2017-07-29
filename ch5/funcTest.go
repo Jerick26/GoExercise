6b1c64ea80cf43530ff7f65ccb9b842c4e4af89e
Jerick26 <jerry.pin26@yahoo.com>
2017-07-29 10:00:25 +0800

    feat(func): update funcTest.go, contain function value

--- ch5/funcTest.go
+++ ch5/funcTest.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"math"
+	"net/http"
+	"strings"
+
+	"golang.org/x/net/html"
 )
 
 func hypot(x, y float64) float64 {
 	return math.Sqrt(x*x + y*y)
 }
 
-func findLinksLog(url string) ([]string, error) {
-	log.Printf("findLinks %s", url)
-	return findLinks(url)
-}
+//func findLinksLog(url string) ([]string, error) {
+//	log.Printf("findLinks %s", url)
+//	return findLinks(url)
+//}
 
 func f(i, j, k int, s, t string) { /* ... */ }
 func add(x int, y int) int       { return x + y }
@@ -23,9 +26,15 @@ func zero(int, int) int          { return 0 }
 
 // Names are particularly valuable when a function retur ns multiple results
 // of the same type
-func Size(rect image.Rectangle) (width, height int)     {}
-func Split(path string) (dir, file string)              {}
-func HourMinSec(t time.Time) (hour, minute, second int) {}
+//func Size(rect image.Rectangle) (width, height int)     {}
+//func Split(path string) (dir, file string)              {}
+//func HourMinSec(t time.Time) (hour, minute, second int) {}
+
+func square(n int) int     { return n * n }
+func negative(n int) int   { return -n }
+func product(m, n int) int { return m * n }
+
+func add1(r rune) rune { return r + 1 }
 
 func main() {
 	fmt.Println(hypot(3, 4))  // "5"
@@ -35,9 +44,25 @@ func main() {
 	fmt.Printf("%T\n", zero)  // "func(int, int) int"
 	fmt.Printf("%.6f\n", math.Sin(30))
 	// test multi-valued
-	log.Println(findLinks(url)) // rarely used in pro duction code
+	//log.Println(findLinks(url)) // rarely used in pro duction code
 	//links, err := findLinks(url)
 	//log.Println(links, err)
+	// Function Values
+	f := square
+	fmt.Println(f(3)) // "9"
+	f = negative
+	fmt.Println(f(3))     // "-3"
+	fmt.Printf("%T\n", f) // "func(int) int"
+	//f = product           // compile error: can't assign f(int, int) int to f(int) int
+	var f2 func(int) int
+	//f2(3) // panic: call of nil function
+	if f2 != nil {
+		f2(3)
+	}
+	// parameterize functions
+	fmt.Println(strings.Map(add1, "HAL-9000")) // "IBM.:111"
+	fmt.Println(strings.Map(add1, "VMS"))      // "WNT"
+	fmt.Println(strings.Map(add1, "Admix"))    // "Benjy"
 }
 
 // CountWordsAndImages does an HTTP GET request for the HTML
@@ -47,14 +72,16 @@ func CountWordsAndImages(url string) (words, images int, err error) {
 	if err != nil {
 		return
 	}
-	doc, err := html.Parse(resp.Body)
+	//doc, err := html.Parse(resp.Body)
 	resp.Body.Close()
 	if err != nil {
 		err = fmt.Errorf("parsing HTML: %s", err)
 		return
 	}
-	word, images = countWordsAndImages(doc)
+	//word, images = countWordsAndImages(doc)
 	return
 }
 
-func countWordsAndImages(n *html.Node) (words, images int) { /* ... */ }
+func countWordsAndImages(n *html.Node) (words, images int) {
+	return 10, 20
+}
